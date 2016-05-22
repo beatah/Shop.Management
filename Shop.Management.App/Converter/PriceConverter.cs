5d6a7dba96ac88bb8e2c62bea79f8a628b1bd893
beatah <b.hryniewicka95@gmail.com>
2016-05-22 10:53:39 +0200

    add price converter

---
+++ Shop.Management.App/Converter/PriceConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace Shop.Management.App.Converter
+{
+    public class PriceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "$" + Decimal.Round(Decimal.Parse(value.ToString()),2);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
